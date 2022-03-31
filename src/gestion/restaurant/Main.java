9c53760c8e484eec2963f46407177cba59fbd0d8
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-03-31 15:02:09 +0100

    update

--- src/gestion/restaurant/Main.java
+++ src/gestion/restaurant/Main.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 public class Main {
 	
 	public static void main(String[] args) {
-		Client c = new Client("Doe", "john", LocalDate.of(2022, 11, 31), "john.doe@gmail.com", "12345677");
+		Client c = new Client("Doe", "john", LocalDate.of(2022, 11, 30), "john.doe@gmail.com", "12345677");
 		Table t = new Table(1,4,"OverSea",5.5);
-		ArrayList<Food> foods = new ArrayList<>();
-		foods.add(new Plat("Pizza", 12));
-		foods.add(new Plat("Pasta", 11));
-		foods.add(new Plat("sushi", 25));
-		foods.add(new Entree("Soupe", 6));
-		foods.add(new Entree("Salde", 7));
-		foods.add(new Dessert("Glace Chocolat", 5));
-		foods.add(new Entree("Cake", 8));
+		ArrayList<Food> foodsMenu = new ArrayList<>();
+		foodsMenu.add(new Plat("Pizza", 12));
+		foodsMenu.add(new Plat("Pasta", 11));
+		foodsMenu.add(new Plat("sushi", 25)); 
+		foodsMenu.add(new Entree("Soupe", 6));
+		foodsMenu.add(new Entree("Salde", 7));
+		foodsMenu.add(new Dessert("Glace Chocolat", 5));
+		foodsMenu.add(new Entree("Cake", 8));
 	}
 
 }
