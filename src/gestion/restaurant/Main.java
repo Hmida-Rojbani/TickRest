d7b61e398cc9eda371ffeb899f4a0fb70d40ace4
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-01 09:20:24 +0100

    update

--- src/gestion/restaurant/Main.java
+++ src/gestion/restaurant/Main.java
@@ -10,12 +10,14 @@ public class Main {
 		Table t = new Table(1,4,"OverSea",5.5);
 		ArrayList<Food> foodsMenu = new ArrayList<>();
 		foodsMenu.add(new Plat("Pizza", 12));
-		foodsMenu.add(new Plat("Pasta", 11));
+ 		foodsMenu.add(new Plat("Pasta", 11));
 		foodsMenu.add(new Plat("sushi", 25)); 
 		foodsMenu.add(new Entree("Soupe", 6));
 		foodsMenu.add(new Entree("Salde", 7));
 		foodsMenu.add(new Dessert("Glace Chocolat", 5));
 		foodsMenu.add(new Dessert("Cake", 8));
+		Ticket tic = new Ticket(1, 2, c, t, foodsMenu);
+		System.out.println(tic);
 	}
 
 }
--- src/gestion/restaurant/Ticket.java
+++ src/gestion/restaurant/Ticket.java
@@ -13,5 +13,25 @@ public class Ticket {
 	private Client client;
 	private Table table;
 	private ArrayList<Food> foods;
+	public Ticket(int numero, int nbCouvert, Client client, Table table, ArrayList<Food> foods) {
+		super();
+		this.numero = numero;
+		this.nbCouvert = nbCouvert;
+		this.client = client;
+		this.table = table;
+		this.foods = foods;
+		this.date = LocalDateTime.now();
+		this.addition = table.getSupplment();
+		for (Food food : foods) {
+			this.addition+=food.getPrix();
+		}
+	}
+	@Override
+	public String toString() {
+		return "Ticket [numero=" + numero + ", date=" + date + ", nbCouvert=" + nbCouvert + ", addition=" + addition
+				+ ", client=" + client + ", table=" + table + ", foods=" + foods + "]";
+	}
+	
+	
 
 }
